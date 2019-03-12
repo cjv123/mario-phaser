5a715fe09e25a8780e2c7cbe9fd188d719f0a688
cjv123 <cjv123@qq.com>
2019-03-12 15:05:24 +0800

    mario-phaser creator

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import 'p2';
+import 'pixi';
+import 'phaser';
+
+import * as WebFontLoader from 'webfontloader';
+
+import Boot from './states/boot';
+import Preloader from './states/preloader';
+import Title from './states/title';
+import * as Utils from './utils/utils';
+import * as Assets from './assets';
+
+class App extends Phaser.Game {
+    constructor(config: Phaser.IGameConfig) {
+        super (config);
+
+        this.state.add('boot', Boot);
+        this.state.add('preloader', Preloader);
+        this.state.add('title', Title);
+
+        this.state.start('boot');
+    }
+}
+
+function startApp(): void {
+    let gameWidth: number = 320;//320
+    let gameHeight: number = 240;//240
+
+    // There are a few more options you can set if needed, just take a look at Phaser.IGameConfig
+    let gameConfig: Phaser.IGameConfig = {
+        width: gameWidth,
+        height: gameHeight,
+        renderer: Phaser.AUTO,
+        parent: '',
+        resolution: 1
+    };
+
+    let app = new App(gameConfig);
+}
+
+window.onload = () => {
+    startApp();
+};
---
+++ src/states/preloader.ts
@@ -0,0 +1,17 @@
+import * as Assets from '../assets';
+import * as AssetUtils from '../utils/assetUtils';
+
+export default class Preloader extends Phaser.State {
+    public preload(): void {
+
+    }
+
+    private waitForSoundDecoding(): void {
+    }
+
+    private startGame(): void {
+    }
+
+    private loadTitle(): void {
+    }
+}
